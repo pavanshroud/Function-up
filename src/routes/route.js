a49e3c2c4cb56e35e607588a5fe7da2df605437d
pavanshroud <kurapatipavan1997@gmail.com>
2022-08-17 00:15:33 +0530

    UPDATED

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,14 @@
 const express = require('express');
-
 const router = express.Router();
+const bookModel = require('../models/bookModel');
+const bookController = require('../controllers/bookController');
 
 router.get('/test-me', function (req, res) {
-    res.send('My first ever api!')
+  res.send('My first ever api!');
 });
 
+router.post('/createBooks', bookController.createBooks);
+
+router.get('/getBooks', bookController.getBooks);
+
 module.exports = router;
-// adding this comment for no reason
