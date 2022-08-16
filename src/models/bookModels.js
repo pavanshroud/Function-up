8f2fe41aa8436519b8c9753b202d789095a44c4b
pavanshroud <kurapatipavan1997@gmail.com>
2022-08-17 00:04:37 +0530

    Updated bookModel

--- src/models/bookModels.js
+++ src/models/bookModels.js
@@ -2,11 +2,26 @@ const mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema(
   {
-    bookName: { type: String, require: true },
-    bookAuthor: String,
-    category: String,
-    year: Number,
+    bookName: {
+      type: String,
+      require: true,
+    },
+    authorName: String,
+    tags: [String],
+
+    prices: {
+      indianPrice: String,
+      europePrice: String,
+    },
+    year: {
+      type: Number,
+      default: '2021',
+      require: true,
+    },
+    totalPages: Number,
+    stockAvailable: Boolean,
   },
+
   { timestamps: true }
 );
 
