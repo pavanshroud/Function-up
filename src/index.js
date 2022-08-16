d944a5bfe5bc9e38a50d98f56bbe9dd3aac36960
pavanshroud <kurapatipavan1997@gmail.com>
2022-08-16 23:45:32 +0530

    installed mongodb

--- src/index.js
+++ src/index.js
@@ -2,14 +2,27 @@ const express = require('express');
 var bodyParser = require('body-parser');
 
 const route = require('./routes/route.js');
+const { default: mongoose } = require('mongoose');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+mongoose
+  .connect(
+    'mongodb+srv://functionup-cohort:9Kuhofudet3PqxOY@cluster0.utetgsi.mongodb.net/Pavan3819-DB?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+    }
+  )
+  .then(() => {
+    console.log('MongoDb is connected');
+  })
+  .catch((err) => console.log(err));
+
 app.use('/', route);
 
-app.listen(process.env.PORT || 3000, function() {
-    console.log('Express app running on port ' + (process.env.PORT || 3000))
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Express app running on port ' + (process.env.PORT || 3000));
 });
